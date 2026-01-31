08db7343783f9129065a087171d472f52b5ca361
Michl Bayer <michlbayer1@gmail.com>
2026-01-31 13:46:03 +0100

    ddfa

--- Assets/Features/Tacho/Tacho.cs
+++ Assets/Features/Tacho/Tacho.cs
@@ -6,10 +6,12 @@ public class Tacho : MonoBehaviour
     [SerializeField,Range(0,1)] private float tachoMeter;
     [SerializeField] private int maxAngle;
     [SerializeField] private Transform tachoNeedle;
-    
-
+    [SerializeField] private int startAngle = 90;
+    [SerializeField] private Rigidbody carRigidBody;
     private void Update()
     {
-        tachoNeedle.localRotation = Quaternion.Euler(0, 0, -tachoMeter * maxAngle);
+        tachoMeter = carRigidBody.linearVelocity.magnitude;
+        
+        tachoNeedle.localRotation = Quaternion.Euler(0, 0, (-tachoMeter * maxAngle) - startAngle);
     }
 }

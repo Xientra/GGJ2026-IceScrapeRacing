abbd3474f58b3da2a14f37dd9444fce83ff99220
Michl Bayer <michlbayer1@gmail.com>
2026-02-01 12:34:46 +0100

    main menue

--- Assets/Features/MainMenue/MainMenue.cs
+++ Assets/Features/MainMenue/MainMenue.cs
@@ -1,16 +1,48 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MainMenue : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Transform mainMenuCamTra;
+    [SerializeField] private GameObject HandL;
+    [SerializeField] private GameObject HandR;
+    [SerializeField] private DOTweenPath dotPath;
+    [SerializeField] private float duration = 3;
+    private Camera _camera;
+
+    [SerializeField] private UnityEvent OnActivateNormalGame;
+
     void Start()
     {
-        
+        _camera = Camera.main;
+
+        _camera.transform.position = mainMenuCamTra.position;
+        _camera.transform.rotation = mainMenuCamTra.rotation;
+
+        HandL.SetActive(false);
+        HandR.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartPressed()
     {
-        
+        _camera.transform.DOPath(dotPath.GetDrawPoints(), duration, PathType.CatmullRom)
+            .SetOptions(AxisConstraint.None, AxisConstraint.X)
+            .SetEase(Ease.OutExpo).Play()
+            .onComplete += ActivateNormalGame;
+
+
+        HandL.SetActive(true);
+        HandR.SetActive(true);
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+
+    public void ActivateNormalGame()
+    {
+        OnActivateNormalGame.Invoke();
     }
 }
